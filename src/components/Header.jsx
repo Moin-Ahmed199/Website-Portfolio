0bb24672597cf6543b4d3108bbbc2e4802353aea
Moin-Ahmed199 <moinahmedgh199@gmail.com>
2025-02-21 02:20:32 -0800

    pushing first code

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css';
+
+
+const Header = () => {
+
+return (
+
+<div className="header-container">
+
+<h1>Buildify Web Service</h1>
+<button>Hello</button>
+</div>
+
+)
+
+}
+
+
+export default Header;
